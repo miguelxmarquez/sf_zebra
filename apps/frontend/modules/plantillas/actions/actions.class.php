b486b68cf3c9006f2d58643eb76ea6c3baa76a17
miguelxmarquez <miguelxmarquez@gmail.com>
2018-03-29 15:38:11 -0500

    Updating core for PHP 7.0

--- apps/frontend/modules/plantillas/actions/actions.class.php
+++ apps/frontend/modules/plantillas/actions/actions.class.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once dirname(__FILE__).'/../lib/plantillasGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/plantillasGeneratorHelper.class.php';
+
 /**
  * plantillas actions.
  *
@@ -8,72 +11,6 @@
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class plantillasActions extends sfActions
+class plantillasActions extends autoPlantillasActions
 {
-  public function executeIndex(sfWebRequest $request)
-  {
-    $this->plantillass = Doctrine_Core::getTable('Plantillas')
-      ->createQuery('a')
-      ->execute();
-  }
-
-  public function executeShow(sfWebRequest $request)
-  {
-    $this->plantillas = Doctrine_Core::getTable('Plantillas')->find(array($request->getParameter('id')));
-    $this->forward404Unless($this->plantillas);
-  }
-
-  public function executeNew(sfWebRequest $request)
-  {
-    $this->form = new PlantillasForm();
-  }
-
-  public function executeCreate(sfWebRequest $request)
-  {
-    $this->forward404Unless($request->isMethod(sfRequest::POST));
-
-    $this->form = new PlantillasForm();
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('new');
-  }
-
-  public function executeEdit(sfWebRequest $request)
-  {
-    $this->forward404Unless($plantillas = Doctrine_Core::getTable('Plantillas')->find(array($request->getParameter('id'))), sprintf('Object plantillas does not exist (%s).', $request->getParameter('id')));
-    $this->form = new PlantillasForm($plantillas);
-  }
-
-  public function executeUpdate(sfWebRequest $request)
-  {
-    $this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
-    $this->forward404Unless($plantillas = Doctrine_Core::getTable('Plantillas')->find(array($request->getParameter('id'))), sprintf('Object plantillas does not exist (%s).', $request->getParameter('id')));
-    $this->form = new PlantillasForm($plantillas);
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('edit');
-  }
-
-  public function executeDelete(sfWebRequest $request)
-  {
-    $request->checkCSRFProtection();
-
-    $this->forward404Unless($plantillas = Doctrine_Core::getTable('Plantillas')->find(array($request->getParameter('id'))), sprintf('Object plantillas does not exist (%s).', $request->getParameter('id')));
-    $plantillas->delete();
-
-    $this->redirect('plantillas/index');
-  }
-
-  protected function processForm(sfWebRequest $request, sfForm $form)
-  {
-    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-    if ($form->isValid())
-    {
-      $plantillas = $form->save();
-
-      $this->redirect('plantillas/edit?id='.$plantillas->getId());
-    }
-  }
 }
---
+++ apps/frontend/modules/plantillas/lib/plantillasGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * plantillas module helper.
+ *
+ * @package    zebra
+ * @subpackage plantillas
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class plantillasGeneratorHelper extends BasePlantillasGeneratorHelper
+{
+}
