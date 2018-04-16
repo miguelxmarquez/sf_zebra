4b4a0817e05482d42f66cb74772b3a310eb11af8
Miguel Márquez <miguelxmarquez@gmail.com>
2018-04-16 14:42:44 -0500

    Adding Monolog and Qz Libs

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+
+require_once __DIR__.'/../vendor/autoload.php';
+
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
---
+++ vendor/qz/lib/Qz/Qz.php
@@ -0,0 +1,11 @@
+<?php
+
+# vendor/qz/lib/Qz/Qz.php
+
+require_once __DIR__.'/src/qz.php';
+
+class Qz extends Qz {
+}
+
+
+ ?>
