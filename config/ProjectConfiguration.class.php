5c9552cb22f95950e5e5216dd1ea7b4b37860aa4
Miguel Márquez <miguelxmarquez@gmail.com>
2018-04-16 14:52:41 -0500

    Adding Composer Auload

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -2,6 +2,7 @@
 
 require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 
+// Composer Autoload
 require_once __DIR__.'/../vendor/autoload.php';
 
 sfCoreAutoload::register();
